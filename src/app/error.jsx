94dd309032965870fc8cb9055a699b72bf1e2012
Ronit Jena <ronitjena64@gmail.com>
2024-06-01 18:34:07 +0530

    error handling

---
+++ src/app/error.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React, { useEffect } from 'react';
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div className='text-center mt-10'>
+      <h1>Something went wrong, please try again later.</h1>
+      <button className='hover:text-amber-400' onClick={reset}>Try Again</button>
+    </div>
+  );
+}
